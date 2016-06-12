dab1b497bb452ab14be16bb530c7ff53b41465e4
matthewwolfe <matthew.wolfe2@gmail.com>
2016-06-12 15:59:52 -0400

    set up initialization of storage in appData

--- src/js/core/StorageManager.js
+++ src/js/core/StorageManager.js
@@ -4,13 +4,43 @@ class StorageManager extends Core {
         super();
 
         this.fs = require('fs');
+        this.path = require('electron').remote.app.getPath('appData');
+        this.directory = 'Soundcloud-Client';
+        this.file = 'data';
+
         this.data = {};
+
+        this.initializeStorage();
 
         this.readFromStorage(function(data){
             this.data = data;
             callback();
 
         }.bind(this));
+    }
+
+    initializeStorage(){
+        let directoryPath = this.path + '/' + this.directory;
+        let filePath = directoryPath + '/' + this.file;
+
+        // initialize the directory
+        if(!this.directoryOrFileExists(directoryPath)){
+            this.fs.mkdirSync(directoryPath);
+        }
+        // initialize the file
+        if(!this.directoryOrFileExists(filePath)){
+            this.writeToStorage();
+        }
+    }
+
+    directoryOrFileExists(path){
+        try {
+            this.fs.accessSync(path, this.fs.F_OK);
+            return true;
+        } catch(err){
+            console.log(err);
+            return false;
+        }
     }
 
     get(property){
@@ -32,7 +62,7 @@ class StorageManager extends Core {
     }
 
     writeToStorage(){
-        this.fs.writeFile('./storage/data', JSON.stringify(this.data), 'utf8', function(err){
+        this.fs.writeFileSync(this.path + '/Soundcloud-Client/data', JSON.stringify(this.data), 'utf8', function(err){
             if(err){
                 console.log(err);
             }
@@ -40,7 +70,7 @@ class StorageManager extends Core {
     }
 
     readFromStorage(callback){
-        this.fs.readFile('./storage/data', 'utf8', function(err, data){
+        this.fs.readFile(this.path + '/Soundcloud-Client/data', 'utf8', function(err, data){
 
             if(err){
                 console.log(err);
