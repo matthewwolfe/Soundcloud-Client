6b284046738309f5000960b2df7726df87b5a971
matthewwolfe <matthew.wolfe2@gmail.com>
2016-07-04 23:14:22 -0400

    add track scroller

--- src/js/views/trackController.jsx
+++ src/js/views/trackController.jsx
@@ -5,7 +5,12 @@ class TrackController extends React.Component {
     constructor(props){
         super(props);
 
-        this.state = {track: {}};
+        this.state = {
+            track: {},
+            playing: false,
+            title_position: 0,
+            isScrolling: false
+        };
 
         window.messenger.subscribe('music-state-change', function(data){
             this.setState({playing: data.playing});
@@ -14,15 +19,53 @@ class TrackController extends React.Component {
                 this.setState({track: window.music.currentlyPlaying.track});
             }
         }.bind(this));
+
+        this.timer = null;
+    }
+
+
+    handleMouseOver(){
+        if(!this.state.isScrolling){
+            let titleWidth = document.getElementById('track-title').scrollWidth;
+
+            if(titleWidth > 220){
+                this.setScroll(titleWidth);
+                this.setState({isScrolling: true});
+            }
+        }
+    }
+
+    setScroll(titleWidth){
+        this.timer = setInterval(function(){
+            if(this.state.title_position <= -(titleWidth + 5)){
+                this.setState({title_position: 220});
+
+            }
+            if(this.state.title_position === 1){
+                this.setState({title_position: 0, isScrolling: false});
+                clearInterval(this.timer);
+            }
+
+            this.setState({title_position: this.state.title_position - 1});
+        }.bind(this), 40);
     }
 
     render () {
         if(this.state.playing){ 
             return (
-                <div id="track-controller">
-                    <img id="track-image" src={this.state.track.artwork_url} />
-                    <div id="track-title">{this.state.track.title}</div>
-                    <div id="track-creator">{this.state.track.user.username}</div>
+                <div id="track-controller-container">
+                    <div id="track-controller">
+                        <img id="track-image" src={this.state.track.artwork_url} />
+
+                        <div id="track-title"
+                             style={{marginLeft: this.state.title_position}}
+                             onMouseOver={this.handleMouseOver.bind(this)}>
+
+                            {this.state.track.title}
+                        </div>
+
+                        <div id="track-creator">{this.state.track.user.username}</div>
+                    </div>
                 </div>
             );
         } else {
