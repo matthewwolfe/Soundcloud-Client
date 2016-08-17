bf0fdac7f5650da4486e50901d06f4c8d751943b
matthewwolfe <matthew.wolfe2@gmail.com>
2016-08-16 22:44:08 -0400

    make the queue list a stateless component

--- src/js/components/queue/queueList.jsx
+++ src/js/components/queue/queueList.jsx
@@ -1,32 +1,29 @@
-import React from 'react';
-
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 import QueueItem from './queueItem.jsx';
 
-class QueueList extends React.Component {
+const QueueList = (props) => {
+    let queueItems = [];
 
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        let queueItems = [];
-
-        for(var i = 0; i < this.props.queue.length; i++){
-            queueItems.push(
-                <QueueItem data={this.props.queue[i]}
-                           key={i} queueOrder={i+1}/>
-            );
-        }
-
-        return (
-            <div id="queue-list" className={this.props.hidden ? 'hide' : ''}>
-                {queueItems}
-            </div>
+    for(var i = 0; i < props.queue.length; i++){
+        queueItems.push(
+            <QueueItem data={props.queue[i]}
+                       key={i} queueOrder={i+1}/>
         );
     }
-}
+
+    return (
+        <div id="queue-list" className={props.hidden ? 'hide' : ''}>
+            {queueItems}
+        </div>
+    );
+};
+
+QueueList.propTypes = {
+    queue: PropTypes.array.isRequired,
+    hidden: PropTypes.bool.isRequired
+};
 
 const getQueueTracks = (queue, tracks) => {
     return queue.map((id) => tracks.filter((track) => track.id === id)[0]);
